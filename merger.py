45c325fd5c9179a0e4fdcc0d5032db398fc03a8a
Hector Fugihara Kroes <hector.kroes@outlook.com>
2023-08-13 18:09:46 -0300

    Implementing command line

--- merger.py
+++ merger.py
@@ -1,33 +1,34 @@
 import pandas as pd
 import sys
 
-excel_file_path = 'teste.xlsx'
-identifier_columns = ['Var1', 'Var2']
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+identifier_columns = sys.argv[3:]
 
-xls = pd.ExcelFile(excel_file_path)
+xls = pd.ExcelFile(input_file)
 
 sheet_names = xls.sheet_names
-patient_data = {}
+line_data = {}
 
 for sheet_name in sheet_names:
     df = pd.read_excel(xls, sheet_name)
     for _, row in df.iterrows():
-        patient_key = []
+        line_key = []
         for identifier in identifier_columns:
-            patient_key.append(row[identifier])
-        patient_key = tuple(patient_key)
+            line_key.append(row[identifier])
+        line_key = tuple(line_key)
         
-        if patient_key not in patient_data:
-            patient_data[patient_key] = {}
+        if line_key not in line_data:
+            line_data[line_key] = {}
             
         for column in df.columns:
             if column not in identifier_columns:
-                patient_data[patient_key][column] = row[column]
+                line_data[line_key][column] = row[column]
 
-merged_df = pd.DataFrame.from_dict(patient_data, orient='index')
+merged_df = pd.DataFrame.from_dict(line_data, orient='index')
 merged_df.reset_index(inplace=True)
 
 for i in range(len(identifier_columns)):
     merged_df.rename(columns={f'level_{i}': identifier_columns[i]}, inplace=True)
 
-merged_df.to_excel('merged_data.xlsx', index=False)
+merged_df.to_excel(output_file, index=False)
